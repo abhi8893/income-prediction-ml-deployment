6ac97be40ee364da2cdb6779fee27f0db5b4d773
abhi8893 <35069768+abhi8893@users.noreply.github.com>
2021-07-13 21:23:03 +0530

    add INPUT_HEADERS in project.config

--- project/config.py
+++ project/config.py
@@ -4,3 +4,20 @@ PROJECT_DIR = Path(__file__).parents[1]
 DATA_HEADERS = ['age','workclass', 'fnlwgt', 'education', 'education-num',
                 'marital-status', 'occupation', 'relationship', 'race', 'sex',
                 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+
+
+INPUT_HEADERS = {
+    'randomforest_best_pipe.pkl': ['age',
+                                   'workclass',
+                                   'fnlwgt',
+                                   'education',
+                                   'education-num',
+                                   'marital-status',
+                                   'occupation',
+                                   'relationship',
+                                   'race',
+                                   'sex',
+                                   'capital-gain',
+                                   'capital-loss',
+                                   'hours-per-week',
+                                   'native-country']}
