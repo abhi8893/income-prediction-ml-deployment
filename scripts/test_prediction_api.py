7d4976f90c619157a28ed83b2c04a6b4f865cd16
abhi8893 <35069768+abhi8893@users.noreply.github.com>
2021-07-13 21:29:06 +0530

    add api prediction test script for data and file

---
+++ scripts/test_prediction_api.py
@@ -0,0 +1,37 @@
+from flask_app import HEADERS
+import requests
+from project.utils import load_data
+from src.utils import remove_extra_whitespace
+from project.config import INPUT_HEADERS, PROJECT_DIR
+import os
+import sys
+import io
+
+PREDICTION_TYPE = sys.argv[1]
+PORT = 9696
+HEADERS = INPUT_HEADERS['randomforest_best_pipe.pkl']
+DATA = load_data('test')
+
+if PREDICTION_TYPE == 'data':
+    URL = f'http://127.0.0.1:{PORT}/predict'
+
+    str_cols = DATA.select_dtypes('object').columns
+    person_features = DATA.iloc[0:10].copy()
+    person_features[str_cols] = person_features[str_cols].applymap(remove_extra_whitespace)
+
+    person_features = person_features[HEADERS].to_dict(orient='list')
+    resp = requests.post(URL, json=person_features)
+
+elif PREDICTION_TYPE == 'file':
+    URL = f'http://127.0.0.1:{PORT}/predict_file'
+
+    s_buf = io.StringIO()
+    DATA[HEADERS].to_csv(s_buf, index=False)
+    s_buf.seek(0)
+
+    resp = requests.post(URL, files={'file': s_buf})
+    
+    # with open(fpath, 'rb') as file:
+    #     resp = requests.post(URL, files={'file': file})
+
+print(resp.text)
