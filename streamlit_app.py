9530a9b074f1f861f0480dfa534bbb66d1482a48
abhi8893 <35069768+abhi8893@users.noreply.github.com>
2021-07-14 21:11:41 +0530

    restructure streamlit app to have two column layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,32 +34,36 @@ def predict(input_features):
 
 def main():
 
-    st.title('Income Prediction using Machine Learning', )
+    # st.title('Census Income Prediction')
     html_temp = """
+    <h1 style="color: white; text-align: center;">Census Income Prediction</h1>
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Income Prediction ML App</h2>
+    <h2 style="color:white; text-align:center;">Income Prediction ML App</h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
+    st.markdown('***')
 
     # input_features = dict()
     # for i, name in enumerate(INPUT_HEADERS):
     #     input_features[name] = st.text_input(name, 'Type Here', key=name)
 
-    age = st.number_input("age", min_value=DATA_STATS.loc['age', 'stats'][0], max_value=DATA_STATS.loc['age', 'stats'][1])
-    workclass = st.selectbox('workclass', DATA_STATS.loc['workclass', 'stats'])
-    fnlwgt = st.number_input("fnlwgt", min_value=DATA_STATS.loc['fnlwgt', 'stats'][0], max_value=DATA_STATS.loc['fnlwgt', 'stats'][1])
-    education = st.selectbox("education", DATA_STATS.loc['education', 'stats'])
-    education_num = st.number_input("education-num", min_value=DATA_STATS.loc['education-num', 'stats'][0], max_value=DATA_STATS.loc['education-num', 'stats'][1])
-    marital_status = st.selectbox("marital-status", DATA_STATS.loc['marital-status', 'stats'])
-    occupation = st.selectbox("occupation", DATA_STATS.loc['occupation', 'stats'])
-    relationship = st.selectbox("relationship", DATA_STATS.loc['relationship', 'stats'])
-    race = st.selectbox("race", DATA_STATS.loc['race', 'stats'])
-    sex = st.selectbox("sex", DATA_STATS.loc['sex', 'stats'])
-    capital_gain = st.number_input("capital-gain", min_value=DATA_STATS.loc['capital-gain', 'stats'][0], max_value=DATA_STATS.loc['capital-gain', 'stats'][1])
-    capital_loss = st.number_input("capital-loss", min_value=DATA_STATS.loc['capital-loss', 'stats'][0], max_value=DATA_STATS.loc['capital-loss', 'stats'][1])
-    hours_per_week = st.number_input("hours-per-week", min_value=DATA_STATS.loc['hours-per-week', 'stats'][0], max_value=DATA_STATS.loc['hours-per-week', 'stats'][1])
-    native_country = st.selectbox("native-country", DATA_STATS.loc['native-country', 'stats'])
+    col1, col2 = st.beta_columns(2)
+
+    age = col1.number_input("age", min_value=DATA_STATS.loc['age', 'stats'][0], max_value=DATA_STATS.loc['age', 'stats'][1])
+    workclass = col1.selectbox('workclass', DATA_STATS.loc['workclass', 'stats'])
+    fnlwgt = col1.number_input("fnlwgt", min_value=DATA_STATS.loc['fnlwgt', 'stats'][0], max_value=DATA_STATS.loc['fnlwgt', 'stats'][1])
+    education = col1.selectbox("education", DATA_STATS.loc['education', 'stats'])
+    education_num = col1.number_input("education-num", min_value=DATA_STATS.loc['education-num', 'stats'][0], max_value=DATA_STATS.loc['education-num', 'stats'][1])
+    marital_status = col1.selectbox("marital-status", DATA_STATS.loc['marital-status', 'stats'])
+    occupation = col1.selectbox("occupation", DATA_STATS.loc['occupation', 'stats'])
+    relationship = col2.selectbox("relationship", DATA_STATS.loc['relationship', 'stats'])
+    race = col2.selectbox("race", DATA_STATS.loc['race', 'stats'])
+    sex = col2.selectbox("sex", DATA_STATS.loc['sex', 'stats'])
+    capital_gain = col2.number_input("capital-gain", min_value=DATA_STATS.loc['capital-gain', 'stats'][0], max_value=DATA_STATS.loc['capital-gain', 'stats'][1])
+    capital_loss = col2.number_input("capital-loss", min_value=DATA_STATS.loc['capital-loss', 'stats'][0], max_value=DATA_STATS.loc['capital-loss', 'stats'][1])
+    hours_per_week = col2.number_input("hours-per-week", min_value=DATA_STATS.loc['hours-per-week', 'stats'][0], max_value=DATA_STATS.loc['hours-per-week', 'stats'][1])
+    native_country = col2.selectbox("native-country", DATA_STATS.loc['native-country', 'stats'])
 
     input_features = dict(zip(HEADERS, [age, workclass, fnlwgt, education, education_num, marital_status, occupation, 
                                    relationship, race, sex, capital_gain, capital_loss, hours_per_week, native_country]))
